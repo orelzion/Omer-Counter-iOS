a0d56fe6b7d90f8f6fa62fc4a774819c8b24bd34
orelzion <orelzion@gmail.com>
2021-04-12 20:52:12 +0300

    Initial Commit

---
+++ Omer Counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Omer Counter
+//
+//  Created by Orel Zion on 12/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Omer Counter/Omer_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Omer_CounterApp.swift
+//  Omer Counter
+//
+//  Created by Orel Zion on 12/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Omer_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
